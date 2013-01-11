04a1c56d3dc987b99709e64da82da726c36250fa
bart <bart@trafficdigital.com>
2013-01-11 12:37:30 +0000

    frontend user functions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,6 +39,7 @@ class AppKernel extends Kernel
             new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
             
             new Traffic\StoreBundle\TrafficStoreBundle(),
+            new Application\FOS\UserBundle\ApplicationFOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test','local','staging'))) {
---
+++ src/Application/FOS/UserBundle/ApplicationFOSUserBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application\FOS\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApplicationFOSUserBundle extends Bundle
+{
+    
+    public function getParent()
+    {
+        return 'ApplicationSonataUserBundle';
+    }
+    
+}
